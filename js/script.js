e358b5de410a9fca4a8a08e7a6be11057da13de7
Saleem Abu Shelbayeh <saleem.abu.shelbaya@gmail.com>
2019-07-17 16:14:47 +0300

    Made the modal work git add .

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 function resetForm() {
   document.getElementById("userForm").reset();
 }
+
+function showModal() {
+  const modal = document.getElementById("modal-wrapper");
+  const form = document.getElementById("userForm");
+  form.onsubmit = modal.style.visibility = "visible";
+}
+
+function hideModal() {
+  document.getElementById("modal-wrapper").style.display = "none";
+}
