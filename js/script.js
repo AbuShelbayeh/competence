7dc07daee260f270c4c666ca226140355aba41f1
Saleem Abu Shelbayeh <saleem.abu.shelbaya@gmail.com>
2019-07-17 16:25:18 +0300

    Removed unused code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
-// function resetForm() {
-//   document.getElementById("userForm").reset();
-// }
+function resetForm() {
+  document.getElementById("userForm").reset();
+}
 
-// function showModal() {
-//   document.getElementById("modal-wrapper").style.display = "flex";
-//   e.preventDefault();
-// }
 function hideModal() {
   document.getElementById("modal-wrapper").style.display = "none";
 }
@@ -13,7 +9,5 @@ function hideModal() {
 $(document).ready(function() {
   $('#userForm').on('submit', function(e){
     document.getElementById("modal-wrapper").style.visibility = "visible";
-    // e.preventDefault();
-    // document.userForm.submit();
   });
 });
